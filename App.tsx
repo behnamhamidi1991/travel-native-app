ab9c680409bcb1400e5b6fed26081a6a90d150c2
Behnam Hamidi <eslp91@hotmail.com>
2024-03-09 18:44:08 +0330

    test

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+import FlatCards from './components/FlatCards';
 
 function App() {
   return (
     <SafeAreaView>
       <ScrollView>
-        <Text>App</Text>
+        <FlatCards />
       </ScrollView>
     </SafeAreaView>
   );
